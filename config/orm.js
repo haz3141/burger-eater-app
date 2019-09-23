0f539022d75eb997be42b3e88a66950def57b947
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-23 14:53:15 -0400

    orm complete

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,74 @@
 const connection = require("../config/connection");
 
+function printQuestionMarks(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  let arr = [];
+  for (let key in ob) {
+    let value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
 const orm = {
-  selectAll : function(table) {
-    const queryString = "SELECT * FROM ??";
-    connection.query(queryString, [table], function(err, result) {
+  selectAll : function(table, cb) {
+    const queryString = "SELECT * FROM " + table + ";";
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
       if (err) throw err;
       console.log(result);
+      // cb(result);
     });
-
   },
-  insertOne : function() {},
-  updateOne : function() {}
+
+  insertOne : function(table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) throw err;
+
+      console.log(result);
+      // cb(result);
+    })
+  },
+
+  updateOne : function(table, objColVals, devoured, cb) {
+    let queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += devoured;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      // cb(result);
+    });
+  }
 };
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const express = require("express");
 const orm = require("./config/orm");
 
 orm.selectAll("burgers");
+
+orm.insertOne("burgers", "burger_name", "Veggie Burger");
