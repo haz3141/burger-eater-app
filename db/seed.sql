0528d4b10058d34cfff1ebcba3dc87be850767ed
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-09-23 13:37:21 -0400

    npm initialization

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Turkey Burger");
